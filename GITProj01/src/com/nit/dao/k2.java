8bcf7fb67e7d71f1d9f4d4df10cf5d7cc1a3a538
biswa <biswa@BISWAJEETPARIDA.mshome.net>
2023-10-03 23:52:52 +0530

    m1- change

--- GITProj01/src/com/nit/dao/k2.java
+++ GITProj01/src/com/nit/dao/k2.java
@@ -2,4 +2,7 @@ package com.nit.dao;
 
 public class k2 {
 //hello
+	//public void display(String msg) {
+		
+	
 }
--- GITProj01/src/com/nit/dao/m1.java
+++ GITProj01/src/com/nit/dao/m1.java
@@ -4,4 +4,7 @@ public class m1 {
 	public void hello() {
 		System.out.println("Hello123");
 	}
+	public void hello1() {
+		System.out.println("hello1");
+	}
 }
